ce2331a7aa6d6b9e41314f0594b1f6445bfcfc5c
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-12 18:05:00 +0100

    20 router setting up

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import Header from "./ui/Header";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./ui/Theme";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Header />
-        hello
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route />
+          </Switch>
+        </BrowserRouter>
       </ThemeProvider>
     );
   }
