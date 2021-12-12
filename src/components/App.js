29997d21b8fccbe7dbdd4e2f4758ce991471f3a7
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-12 11:41:58 +0100

    elevator nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        {[...new Array(12)]
+          .map(
+            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+          )
+          .join("\n")}
       </div>
     );
   }
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,27 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import { Toolbar } from "@material-ui/core";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+function ElevationScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 function Header(props) {
   return (
-    <AppBar>
-      <Toolbar> Arc Development </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar> Arc Development </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 }
 
