0a9a98304ff7cb7eed3127f3e7a9fedf3c7a9d62
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-12 18:10:02 +0100

    20 / 9 routes defined

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,46 @@ class App extends Component {
           <Header />
           <Switch>
             <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
-            <Route exact path="/" component={() => <div>Hoooooome</div>} />
+            <Route
+              exact
+              path="/services"
+              component={() => <div>services</div>}
+            />
+            <Route
+              exact
+              path="/customsoftware"
+              component={() => <div>customsoftware</div>}
+            />
+            <Route
+              exact
+              path="/mobileapps"
+              component={() => <div>mobileapps</div>}
+            />
+            <Route
+              exact
+              path="/websites"
+              component={() => <div>websites</div>}
+            />
+            <Route
+              exact
+              path="/revolution"
+              component={() => <div>revolution</div>}
+            />
+            <Route
+              exact
+              path="/about"
+              component={() => <div>about ussssssss</div>}
+            />
+            <Route
+              exact
+              path="/contact"
+              component={() => <div>contact ussssssss</div>}
+            />
+            <Route
+              exact
+              path="/estimate"
+              component={() => <div>estimate</div>}
+            />
           </Switch>
         </BrowserRouter>
       </ThemeProvider>
