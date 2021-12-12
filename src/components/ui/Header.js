c9cdae1aad18b4d0ffdcbb8ca644e14192a2c764
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-12 12:06:17 +0100

    typography settled up

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import { Toolbar } from "@material-ui/core";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
+import { Typography } from "@material-ui/core";
+
 function ElevationScroll(props) {
   const { children, window } = props;
   const trigger = useScrollTrigger({
@@ -18,8 +20,12 @@ function ElevationScroll(props) {
 function Header(props) {
   return (
     <ElevationScroll>
-      <AppBar position="fixed">
-        <Toolbar> Arc Development </Toolbar>
+      <AppBar position="fixed" color="primary">
+        <Toolbar>
+          <Typography variant="h2" color="secondary">
+            Arc Development
+          </Typography>{" "}
+        </Toolbar>
       </AppBar>
     </ElevationScroll>
   );
