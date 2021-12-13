489519e827979a3e1b862d968c9fe25c391ef52e
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-13 17:35:24 +0100

    context down of nav bar responsive

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -30,6 +30,12 @@ const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
     marginBottom: "3em",
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "2em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.25em",
+    },
   },
   logo: {
     height: "8em",
