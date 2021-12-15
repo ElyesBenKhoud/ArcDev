d3d1f9699fbceb318fca344c10e773aca2d23888
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-15 15:13:40 +0100

    footer handling setup

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: "#0B72B9",
+  },
+}));
+export default function Footer() {}
