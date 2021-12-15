5a01033a54b3f1e040bc48b71580bfaf9f1b81ad
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-15 15:32:17 +0100

    footer grid styling

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import footerAdornment from "../../assets/Footer Adornment.svg";
+import { Grid } from "@material-ui/core";
+
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: "#0B72B9",
@@ -18,12 +20,26 @@ const useStyles = makeStyles((theme) => ({
       width: "15em",
     },
   },
+  mainContainer: {
+    position: "absolute",
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+  },
 }));
 export default function Footer() {
   const classes = useStyles();
 
   return (
     <footer className={classes.footer}>
+      <Grid container className={classes.mainContainer}>
+        <Grid item className={classes.link}>
+          Home
+        </Grid>
+      </Grid>
       <img
         alt="deco slash"
         src={footerAdornment}
