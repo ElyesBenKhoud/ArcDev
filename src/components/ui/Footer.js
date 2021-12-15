892b6347cdc1cb140102764d356a8941a5c3e9ca
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-15 15:23:23 +0100

    footer v2.1.2

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-
+import footerAdornment from "../../assets/Footer Adornment.svg";
 const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: "#0B72B9",
     width: "100%",
   },
+  adornment: {
+    width: "25em",
+  },
 }));
 export default function Footer() {
   const classes = useStyles();
 
-  return <footer className={classes.footer}>example footer</footer>;
+  return (
+    <footer className={classes.footer}>
+      <img
+        alt="deco slash"
+        src={footerAdornment}
+        className={classes.adornment}
+      />
+    </footer>
+  );
 }
