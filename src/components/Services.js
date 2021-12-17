8e16189f5135b3fc83a70eb0045b38ddab0f7ea7
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-17 22:25:17 +0100

    services tables modified

--- src/components/Services.js
+++ src/components/Services.js
@@ -14,16 +14,16 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, calories, fat, carbs) {
+  return { name, calories, fat, carbs };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Win 7.1", 159, 6.0, 24, 4.0),
+  createData("Win 8.0", 237, 9.0, 37, 4.3),
+  createData("Win 10 Pro", 262, 16.0, 24, 6.0),
+  createData("Win 10", 305, 3.7, 67, 4.3),
+  createData("Win 11", 356, 16.0, 49, 3.9),
 ];
 
 export default function Services() {
@@ -34,11 +34,10 @@ export default function Services() {
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell> Services</TableCell>
+            <TableCell align="right">SoftWare Dev</TableCell>
+            <TableCell align="right">Mobile App Dev</TableCell>
+            <TableCell align="right">Website Dev</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -50,7 +49,6 @@ export default function Services() {
               <TableCell align="right">{row.calories}</TableCell>
               <TableCell align="right">{row.fat}</TableCell>
               <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
